8a107d22dc045a84773830e16a5ef7edf8ff30a0
Lennard Schwarz <lennard@konsila.de>
2023-11-05 20:15:59 +0100

    Remove google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
-
-const googleAnalyticsTag = process.env.GOOGLE_ANALYTICS_ID;
 
 const Document: React.FC = () => (
   <Html lang="en">
     <Head>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsTag}`}
-        id="google-analytics"
-        strategy="afterInteractive"
-      >
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${googleAnalyticsTag}');
-        `}
-      </Script>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
